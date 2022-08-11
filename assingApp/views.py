8de937888506c92fcc83d80a8ac0919271d9d47c
Jeimy Arias <tr2303100@gmail.com>
2022-08-11 13:30:30 -0500

    the endowment allocation area finished and small changes are made to devcoApp and devcoProject

--- assingApp/views.py
+++ assingApp/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import assingForm
 from .models import assing
+import urllib.request 
+import json
+import requests
 
 
 # Create your views here.
 
-url  = ''
 def assing(request):
+    url = 'https://mockend.com/tatiana-017/devco_test/posts'
+    responseApi = requests.get(url)
+    dataApi = responseApi.text
+    json_dataApi = json.loads(dataApi)
+    serialApi = []
+
+    for entry in json_dataApi:
+        serialApi.append(entry['id'])
+
+    serialIn = 0
     assing_form=assingForm()
     if request.POST:
         assing_form=assingForm(request.POST, request.FILES)
+        assingData = dict(assing_form.data)
+        serialIn = assingData.get('serial')
+        final = int(serialIn[0])
+
+        if final in serialApi:
+            return redirect("/assing/?invalid")
+
         if assing_form.is_valid():
-            assing_form.save()
+            #assing_form.save()
+            return redirect("/assing/?valid")
         return render(request, "assingApp/assing.html", {'myAssingForm':assing_form})
     return render(request, "assingApp/assing.html", {'myAssingForm':assing_form})
